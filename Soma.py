c77505d5ab8c8733bc32663ff79ed59350a0bcda
JoseKSG <101610140+JoseKSG@users.noreply.github.com>
2022-03-15 22:53:49 -0300

    Add files via upload

---
+++ Soma.py
@@ -0,0 +1,8 @@
+print('='*10,'Soma','='*10)
+
+n1 = float(input('Informe o primeiro número: '))
+n2 = float(input('Informe o segundo número: '))
+
+s = n1 + n2
+
+print('A soma entre {} e {} é {}!'.format(n1, n2, s))
