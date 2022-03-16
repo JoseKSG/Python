22302a41804f1dc6d51bed810da0723266979c9f
JoseKSG <101610140+JoseKSG@users.noreply.github.com>
2022-03-16 13:29:40 -0300

    Add files via upload

---
+++ Antecessor e sucessor.py
@@ -0,0 +1,8 @@
+print('='*10, 'Antecessor e sucessor','='*10)
+
+num = int(input('Digite um número: '))
+
+ant = num - 1
+suc = num + 1
+
+print('Número informado: {}. Seu antecessor é {} e o seu sucessor é {}'.format(num, ant, suc))
