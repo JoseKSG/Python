a914040b1567fdca93279b11aac625e823d2469d
JoseKSG <101610140+JoseKSG@users.noreply.github.com>
2022-03-15 22:30:57 -0300

    Add files via upload

---
+++ Informações.py
@@ -0,0 +1,7 @@
+print('='*10,'Informações','='*10)
+
+nome = input('Qual é o seu nome? ')
+idade = input('Quantos anos você tem? ')
+peso = input('Você pesa quanto? ')
+
+print('Seu nome é {}, tem {} anos e pesa {} kgs!'.format(nome, idade, peso))
